20b65b56fad6784fe867f9a1c9af0ce49f41b109
Adrian Suter <adrian@suter-wirz.ch>
2019-11-23 00:25:46 +0100

    Update headers

--- tests/integration/src/TestClosureOverride/BigBen.php
+++ tests/integration/src/TestClosureOverride/BigBen.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * PHP Autoload Override (https://github.com/adriansuter/php-autoload-override)
+ *
+ * @license https://github.com/adriansuter/php-autoload-override/blob/master/LICENSE.md (MIT License)
+ */
 
 declare(strict_types=1);
 
